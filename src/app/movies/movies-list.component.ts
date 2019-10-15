d54cc310fc33499ab27a5f4ef1e7c0231abb4d0c
IhuomaO <logzimalone@gmail.com>
2019-10-15 11:25:53 +0100

    created ui component

--- src/app/movies/movies-list.component.ts
+++ src/app/movies/movies-list.component.ts
@@ -15,57 +15,93 @@ export class MoviesListComponent {
    movies = [
      {
      id: 1,
-     title: 'Star Wars',
-     imageUrl: '/assets/images/star-wars.png' ,
+     title: 'A Star is born',
+     imageUrl: '/assets/images/Star.jpg' ,
      productionYear: 2018
    },
    {
     id: 2,
-    title: 'Star Wars',
-    imageUrl: '/assets/images/star-wars.png' ,
-    productionYear: 2018
+    title: 'Aquaman',
+    imageUrl: '/assets/images/Aquaman.jpg' ,
+    productionYear: 2019
   },
   {
     id: 3,
-    title: 'Star Wars',
-    imageUrl: '/assets/images/star-wars.png' ,
-    productionYear: 2018
+    title: 'Baby Boy  2',
+    imageUrl: '/assets/images/Baby.png' ,
+    productionYear: 2010
   },
   {
     id: 4,
-    title: 'Star Wars',
-    imageUrl: '/assets/images/star-wars.png' ,
-    productionYear: 2018
+    title: 'Crazy Rich Asians',
+    imageUrl: '/assets/images/Asians.jpg' ,
+    productionYear: 2019
   },
   {
     id: 5,
-    title: 'Star Wars',
-    imageUrl: '/assets/images/star-wars.png' ,
+    title: 'Beauty & The Beast',
+    imageUrl: '/assets/images/Beast.jpg' ,
     productionYear: 2018
   },
   {
     id: 6,
-    title: 'Star Wars',
-    imageUrl: '/assets/images/star-wars.png' ,
-    productionYear: 2018
+    title: 'Creed II',
+    imageUrl: '/assets/images/Creed.jpg' ,
+    productionYear: 2017
   },
   {
     id: 7,
-    title: 'Star Wars',
-    imageUrl: '/assets/images/star-wars.png' ,
+    title: 'Deepwater Horizons',
+    imageUrl: '/assets/images/DeepWater.jpg' ,
     productionYear: 2018
   },
   {
     id: 8,
-    title: 'Star Wars',
-    imageUrl: '/assets/images/star-wars.png' ,
-    productionYear: 2018
+    title: 'GodFather',
+    imageUrl: '/assets/images/Godfather.jpg' ,
+    productionYear: 2006
   },
   {
     id: 9,
-    title: 'Star Wars',
-    imageUrl: '/assets/images/star-wars.png' ,
+    title: 'Home Alone',
+    imageUrl: '/assets/images/Home.jpg' ,
     productionYear: 2018
+  },
+  {
+    id: 10,
+    title: 'John Wick 3',
+    imageUrl: '/assets/images/Wick.jpg' ,
+    productionYear: 2019
+  },
+  {
+    id: 11,
+    title: 'Maleficent',
+    imageUrl: '/assets/images/Maleficent.jpg' ,
+    productionYear: 2015
+  },
+  {
+    id: 12,
+    title: 'Long Walk To Freedom',
+    imageUrl: '/assets/images/Mandela.jpg' ,
+    productionYear: 2013
+  },
+  {
+    id: 13,
+    title: 'The Hangover',
+    imageUrl: '/assets/images/Hangover.jpg' ,
+    productionYear: 2009
+  },
+  {
+    id: 14,
+    title: 'The Lord of the Rings',
+    imageUrl: '/assets/images/Rings.jpg' ,
+    productionYear: 2013
+  },
+  {
+    id: 15,
+    title: 'Wonder Woman',
+    imageUrl: '/assets/images/Wonder.jpg' ,
+    productionYear: 2017
   },
 
   ];
--- src/app/movies/movies-thumbnail.component.ts
+++ src/app/movies/movies-thumbnail.component.ts
@@ -3,10 +3,10 @@ import { Component, Input } from '@angular/core';
 @Component ({
    selector: 'app-movies-thumbnail',
    template: `
-   <div class="card mb-3" style="max-width: 350px;">
+   <div class="card mb-3" style="max-width: 350px, max-height:200px, min-height: 200px;">
       <div class="row no-gutters">
-        <div class="col-md-4">
-          <img src="..." class="card-img" alt="...">
+        <div class="col-md-4 justify-content-center">
+          <img src="{{movies.imageUrl}} " class="card-img"  style="min-height: 100px;" alt="...">
         </div>
         <div class="col-md-8">
           <div class="card-body">
